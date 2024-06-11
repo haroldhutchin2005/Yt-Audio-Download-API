3df14208020663abb34785038154fead0f5510ee
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-06-11 15:56:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const os = require('os');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/yt', async (req, res) => {
     const searchQuery = req.query.search;
